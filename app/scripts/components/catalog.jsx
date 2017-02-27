05253e9331a74c6eb157457010300903880b2b79
Nathan <nathanstarwalt@gmail.com>
2017-02-27 15:31:23 -0500

    Thumbnails populating

--- app/scripts/components/catalog.jsx
+++ app/scripts/components/catalog.jsx
@@ -7,9 +7,9 @@ class CatalogView extends React.Component{
     super();
     var shirtMenu = new models.ShirtCollection();
     var menu = [
-      {name: 't-shirt', sleeves: 'short'},
-      {name: 'long-sleeve t-shirt', sleeves: 'long'},
-      {name: 'tank-top', sleeves: 'none'}
+      {name: 't-shirt', sleeves: 'short', picture: 'http://www.promozionale.net/images/3100001_EX.jpg'},
+      {name: 'long-sleeve t-shirt', sleeves: 'long', picture: 'https://ultrapress-production.s3.amazonaws.com/media/product_inventory/31/bb6150b9-2cf5-48af-950d-bf9597f7ba91.jpg'},
+      {name: 'tank-top', sleeves: 'none', picture: 'http://wheeliejuice.com/wp-content/uploads/2016/02/black-tank-top.jpg'}
     ];
     shirtMenu.add(menu);
     this.state = {
@@ -62,10 +62,31 @@ class LogIn extends React.Component{
 }
 
 class ShirtMenu extends React.Component{
+  constructor(){
+    super();
+
+  }
   render(){
+    var shortList = this.props.data.shirtMenu.toJSON();
+    console.log(shortList);
+    var MenuItems = shortList.map((shirt, index)=>{
+      return(
+        <div className="col-sm-6 col-md-4" key={index}>
+          <div className="thumbnail">
+            <img src={shirt.picture} />
+            <div className="caption">
+              <h3>Thumbnail label</h3>
+              <p>...</p>
+              <p><a href="#" className="btn btn-primary" role="button">Buy</a></p>
+            </div>
+          </div>
+        </div>
+      )
+    });
     return(
+
       <div>
-        Shirt Menu
+        {MenuItems}
       </div>
     )
   }
