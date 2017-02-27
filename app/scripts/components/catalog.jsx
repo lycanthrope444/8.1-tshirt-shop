b8bdceb896c321727a2545a63868e1b0009a684d
Nathan <nathanstarwalt@gmail.com>
2017-02-27 18:05:29 -0500

    Modal demo added

--- app/scripts/components/catalog.jsx
+++ app/scripts/components/catalog.jsx
@@ -95,48 +95,29 @@ class LogIn extends React.Component{
   render(){
     return (
       <div>
-        Login Button
+        <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+          Launch demo modal
+        </button>
+        <div className="modal fade" tabIndex="-1" role="dialog" id="myModal">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 className="modal-title">Modal title</h4>
+              </div>
+              <div className="modal-body">
+                <p>One fine body&hellip;</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
-
-// class ShirtMenu extends React.Component{
-//   constructor(){
-//     super();
-//
-//   }
-//   render(){
-//     // var shortList = this.props.data.shirtMenu.toJSON();
-//     // console.log(shortList);
-//     // var MenuItems = shortList.map((shirt, index)=>{
-//     //   return(
-//     //     <div className="col-sm-6 col-md-4" key={index}>
-//     //       <div className="thumbnail">
-//     //         <img src={shirt.picture} />
-//     //         <div className="caption">
-//     //           <h3>{shirt.name}</h3>
-//     //           <p>{shirt.description}</p>
-//     //           <p>
-//     //             <a href="#"
-//     //               onClick={(e)=>{e.preventDefault(); this.props.addItem(shirt)}}
-//     //               className="btn btn-primary"
-//     //               role="button">
-//     //               Buy</a>
-//     //
-//     //           </p>
-//     //         </div>
-//     //       </div>
-//     //     </div>
-//     //   )
-//     // });
-//     return(
-//
-//       <div>
-//         {MenuItems}
-//       </div>
-//     )
-//   }
-// }
 
 module.exports = CatalogView;
